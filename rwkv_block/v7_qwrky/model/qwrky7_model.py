fc90d6de197db9a4c212461061b2ff2d7fd6aeaa
Eugene Cheah (picocreator) <picocreator@gmail.com>
2025-02-11 21:41:36 +0000

    qwrky7 model

--- rwkv_block/v7_qwrky/model/qwrky7_model.py
+++ rwkv_block/v7_qwrky/model/qwrky7_model.py
@@ -31,6 +31,7 @@ class Qwrky7Model(nn.Module):
         dtype = configMap.get_dtype('bfloat16')
         hidden_size = configMap.hidden_size
         padding_idx = configMap.padding_idx
+        head_size = configMap.head_size
 
         # Embedding layer
         self.embed_tokens = nn.Embedding(vocab_size, hidden_size, padding_idx)
@@ -49,15 +50,14 @@ class Qwrky7Model(nn.Module):
             stateTuneList = [None]*num_hidden_layers
             for i in range(num_hidden_layers):
                 stateTuneList[i] = nn.ParameterDict({
-                    "wkv": nn.Parameter(torch.zeros(hidden_size // 64, 64, 64, device=device, dtype=dtype)),
+                    "wkv": nn.Parameter(torch.zeros(hidden_size // head_size, head_size, head_size, device=device, dtype=dtype)),
                 })
             self.init_state = nn.ParameterList(stateTuneList)
 
     def reset_parameters(self):
         '''
-        Reset the parameters of the block, to an initial state used for training a model from scratch
+        Reset the parameters of the model, to an initial state used for training a model from scratch
         '''
-
         # Get the required prop
         configMap = self.configMap
         num_hidden_layers = configMap.num_hidden_layers
@@ -65,8 +65,9 @@ class Qwrky7Model(nn.Module):
         device = configMap.get_device(None)
         dtype = configMap.get_dtype('bfloat16')
         hidden_size = configMap.hidden_size
+        head_size = configMap.head_size
         
-        # Iterate and reset the blocks
+        # Iterate and reset the layers
         for i in range(num_hidden_layers):
             self.layers[i].reset_parameters()
 
@@ -84,12 +85,12 @@ class Qwrky7Model(nn.Module):
                 stateTuneList = [None]*num_hidden_layers
                 for i in range(num_hidden_layers):
                     stateTuneList[i] = nn.ParameterDict({
-                        "wkv": nn.Parameter(torch.zeros(hidden_size // 64, 64, 64, device=device, dtype=torch.float)),
+                        "wkv": nn.Parameter(torch.zeros(hidden_size // head_size, head_size, head_size, device=device, dtype=torch.float)),
                     })
                 self.init_state = nn.ParameterList(stateTuneList)
             else:
                 for i in range(num_hidden_layers):
-                    self.init_state[i]["wkv"].data.copy_(torch.zeros(hidden_size // 64, 64, 64, device=device, dtype=torch.float))
+                    self.init_state[i]["wkv"].data.copy_(torch.zeros(hidden_size // head_size, head_size, head_size, device=device, dtype=torch.float))
 
 
     def load_from_model_state_dict(self, state_dict: dict, non_blocking:bool=True):
@@ -100,15 +101,22 @@ class Qwrky7Model(nn.Module):
         for i, layers in enumerate(self.layers):
             layers.load_from_model_state_dict(state_dict, i, non_blocking=non_blocking)
 
-        self.embed_tokens
+        self.embed_tokens.weight.data.copy_(state_dict['model.embed_tokens.weight'], non_blocking=non_blocking)
+        self.norm.weight.data.copy_(state_dict['model.norm.weight'], non_blocking=non_blocking)
+        # self.norm.bias.data.copy_(state_dict['model.norm.bias'], non_blocking=non_blocking)
         
+        if self.configMap.init_state_wkv:
+            for i in range(self.configMap.num_hidden_layers):
+                if 'init_state.'+str(i)+'.wkv' in state_dict:
+                    self.init_state[i]["wkv"].data.copy_(state_dict['init_state.'+str(i)+'.wkv'], non_blocking=True)
+
     ### ---
     ###
     ### Init state handling
     ###
     ### ---
 
-    def get_init_state(self, batch_size:int=1, skip_init_state:bool=False) -> list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]:
+    def get_init_state(self, batch_size:int=1, skip_init_state:bool=False) -> list[torch.Tensor]:
         '''
         Get an initialized copy of the model state, for the given batch size
         '''
@@ -116,27 +124,24 @@ class Qwrky7Model(nn.Module):
         hidden_size = self.configMap.hidden_size
         init_state_wkv = self.configMap.init_state_wkv
         num_hidden_layers = self.configMap.num_hidden_layers
+        head_size = self.configMap.head_size
 
         # Prepare the initial state
         init_state = [ None for i in range(num_hidden_layers) ]
         for i in range(num_hidden_layers):
-            device = self.blocks[i].ln1.weight.data.device
-            dtype = self.blocks[i].ln1.weight.data.dtype
+            device = self.layers[i].ln1.weight.data.device
+            dtype = self.layers[i].ln1.weight.data.dtype
 
             # Use the saved init_state if enabled
             # TODO: Consider letting the wkv_state dtype be a parameter
-            wkv_state = torch.zeros(batch_size, hidden_size // 64, 64, 64, device=device, dtype=torch.float)
+            wkv_state = torch.zeros(batch_size, hidden_size // head_size, head_size, head_size, device=device, dtype=torch.float)
             if init_state_wkv and skip_init_state == False:
                 init_wkv = self.init_state[i]["wkv"]
                 for b in range(batch_size):
                     wkv_state[b][:] = init_wkv
 
             # Prepare the state
-            init_state[i] = (
-                torch.zeros(batch_size, hidden_size, device=device, dtype=dtype),
-                wkv_state,
-                torch.zeros(batch_size, hidden_size, device=device, dtype=dtype)
-            )
+            init_state[i] = wkv_state
         return init_state
 
     ### ---
@@ -151,7 +156,7 @@ class Qwrky7Model(nn.Module):
         ret_stateList:list[torch.Tensor] = None,
     ) -> tuple[torch.Tensor,list[torch.Tensor]]:
         '''
-        Forward the block set, given the input tokens and the last state
+        Forward the layer set, given the input tokens and the last state
         Last state is a list of time mix wkv state
 
         Returns a pair of the output embedding and the next state
@@ -168,51 +173,15 @@ class Qwrky7Model(nn.Module):
         # Forward internally
         return self._forward_internal(idx, prv_stateList, ret_stateList, overwrite_ret_tensor=True)
     
-    def _forward_block_hook(self, 
-            block:Qwrky7LayerBlock, 
-            x_hidden_state:torch.Tensor, 
-            prv_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]], 
-            v_first:torch.Tensor
-        ) -> tuple[torch.Tensor,tuple[torch.Tensor,torch.Tensor,torch.Tensor],torch.Tensor]:
-        '''
-        Forward block hook operation, that is easily overridable.
-        To implement gradient checkpointing for use in various trainers
-        '''
-        x_hidden_state = x_hidden_state.to(block.ln1.weight.device, non_blocking=True)
-        return block(x_hidden_state, prv_stateList, v_first)
-
-    def _forward_internal(
-        self, idx:torch.Tensor, 
-        prv_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],  
-        ret_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],
-        overwrite_ret_tensor:bool=False
-    ) -> tuple[torch.Tensor,list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]]:
-        '''
-        Internal forward operations, which assumes the state is already initialized
-        Due to the lack of safety checks, this should not be used directly
-        '''
-        # Lets get the embedding
-        idx = idx.to(self.emb.weight.device, non_blocking=True)
-        x_hidden_state = self.emb(idx)
-
-        # Forward the block layers
-        x_output_embedding, retStateList = self._forward_internal_embeddings(x_hidden_state, prv_stateList, ret_stateList, overwrite_ret_tensor)
-
-        # Perform the head operation
-        x_output = self.head(x_output_embedding.to(self.head.weight.device, non_blocking=True))
-        
-        # Return the output and the state list
-        return x_output, retStateList
-
     def _forward_internal_embeddings(
-        self, x_hidden_state:torch.Tensor, 
-        prv_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],  
-        ret_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],
-        overwrite_ret_tensor:bool=False
-    ) -> tuple[torch.Tensor,list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]]:
+            self, x_hidden_state:torch.Tensor, 
+            prv_stateList:list[torch.Tensor],  
+            ret_stateList:list[torch.Tensor],
+            overwrite_ret_tensor:bool=False
+    ) -> tuple[torch.Tensor,list[torch.Tensor]]:
         '''
         Internal forward operations, which assumes the state is already initialized.
-        And uses the x_hidden_state as the input (generating by the embedding layer)
+        And uses the x_hidden_state as the input (bypassing the embedding layer)
         And returns the output embedding before the head layer
 
         Due to the lack of safety checks, this should not be used directly
@@ -232,27 +201,27 @@ class Qwrky7Model(nn.Module):
         forward_chunk_size = self.configMap.forward_chunk_size
         forward_chunk_count = math.ceil( x_input_length / forward_chunk_size )
 
-        # Iterate the block layers, compute the x_hidden_state
-        for i, block in enumerate(self.blocks):
+        # Iterate the layers, compute the x_hidden_state
+        for i, layer in enumerate(self.layers):
             
             # Single pass, optimized
             if forward_chunk_count <= 1:
-                x_hidden_state, last_block_state, v_first = self._forward_block_hook(block, x_hidden_state, prv_stateList[i], v_first)
+                x_hidden_state, last_layer_state, v_first = self._forward_layer_hook(layer, x_hidden_state, prv_stateList[i], v_first)
             else:
-                # Damn it, we need to chunk
+                # Sadly, we need to chunk
                 new_x_hidden_state_arr = [None]*forward_chunk_count
                 v_first_arr = [None]*forward_chunk_count if v_first is None else None
-                last_block_state = prv_stateList[i]
+                last_layer_state = prv_stateList[i]
 
                 # Iterate the chunks
                 for j in range(forward_chunk_count):
                     start = j * forward_chunk_size
                     endin = min( start + forward_chunk_size, x_input_length )
 
-                    new_x_hidden_state, last_block_state, v_first_part = self._forward_block_hook(
-                        block, 
+                    new_x_hidden_state, last_layer_state, v_first_part = self._forward_layer_hook(
+                        layer, 
                         x_hidden_state[:,start:endin], 
-                        last_block_state, 
+                        last_layer_state, 
                         v_first[:, start:endin] if v_first is not None else None
                     )
 
@@ -266,28 +235,59 @@ class Qwrky7Model(nn.Module):
                 if v_first_arr is not None:
                     v_first = torch.cat(v_first_arr, dim=1)
 
-            # last_block_state = prv_stateList[i]
+            # last_layer_state = prv_stateList[i]
             # Overwrite tensor if needed
             if overwrite_ret_tensor:
-                ret_stateList[i][0][:] = last_block_state[0]
-                ret_stateList[i][1][:] = last_block_state[1]
-                ret_stateList[i][2][:] = last_block_state[2]
+                ret_stateList[i][:] = last_layer_state
             else:
-                ret_stateList[i] = last_block_state
+                ret_stateList[i] = last_layer_state
                 
-        # Final layer norm, and head
+        # Final layer norm, without the head
         x_hidden_state = x_hidden_state.to(self.ln_out.weight.device, non_blocking=True)
         x_hidden_state = self.ln_out(x_hidden_state)
 
         # Return the output and the state list
         return x_hidden_state, ret_stateList
+        
+    def _forward_layer_hook(self, 
+            layer:Qwrky7LayerBlock, 
+            x_hidden_state:torch.Tensor, 
+            prv_stateList:list[torch.Tensor], 
+            v_first:torch.Tensor
+    ) -> tuple[torch.Tensor,torch.Tensor,torch.Tensor]:
+        '''
+        Forward layer hook operation, that is easily overridable.
+        To implement gradient checkpointing for use in various trainers
+        '''
+        x_hidden_state = x_hidden_state.to(layer.input_layernorm.weight.device, non_blocking=True)
+        return layer(x_hidden_state, prv_stateList, v_first)
     
+    def _forward_internal(
+        self, idx:torch.Tensor, 
+        prv_stateList:list[torch.Tensor],  
+        ret_stateList:list[torch.Tensor],
+        overwrite_ret_tensor:bool=False
+    ) -> tuple[torch.Tensor,list[torch.Tensor]]:
+        '''
+        Internal forward operations, which assumes the state is already initialized
+        Due to the lack of safety checks, this should not be used directly
+        '''
+        # Lets get the embedding
+        idx = idx.to(self.embed_tokens.weight.device, non_blocking=True)
+        x_hidden_state = self.embed_tokens(idx)
+
+        # Forward the layer layers
+        x_output_embedding, retStateList = self._forward_internal_embeddings(x_hidden_state, prv_stateList, ret_stateList, overwrite_ret_tensor)
+
+        # Return the output and the state list
+        return x_output_embedding, retStateList
+
     @torch.compile(mode="default")
     def forward_with_default_compile(
         self, idx:torch.Tensor, 
-        prv_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],
-        ret_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]],
-    ) -> tuple[torch.Tensor,list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]]:
+        prv_stateList:list[torch.Tensor],
+        ret_stateList:list[torch.Tensor],
+    ) -> tuple[torch.Tensor,list[torch.Tensor]]:
         '''
         Compiled varient of the forward function
         With no new tensors being created for the output
@@ -299,8 +299,8 @@ class Qwrky7Model(nn.Module):
     @torch.compile(mode="reduce-overhead")
     def forward_with_reduce_compile(
         self, in_idx:torch.Tensor, 
-        prv_stateList:list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]
-    ) -> tuple[torch.Tensor,list[tuple[torch.Tensor,torch.Tensor,torch.Tensor]]]:
+        prv_stateList:list[torch.Tensor]
+    ) -> tuple[torch.Tensor,list[torch.Tensor]]:
         '''
         Compiled varient of the forward function, requires previous state to be passed
         '''
